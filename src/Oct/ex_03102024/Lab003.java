e28d74f7e39abad9ac24a2ebb9a36f129048fec0
Hp <pranavchoudhari7@gmail.com>
2024-10-07 18:17:26 +0530

    All Files are added

---
+++ src/Oct/ex_03102024/Lab003.java
@@ -0,0 +1,12 @@
+package Oct.ex_03102024;
+
+public class Lab003 {
+    public static void main(String[] args) {
+        System.out.println(10+5);// Addition
+        System.out.println(10-5);// subs
+        System.out.println(10*5);// multiplication
+        System.out.println(10/5);// Divide
+
+
+    }
+}
